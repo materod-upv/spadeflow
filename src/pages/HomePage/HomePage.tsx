dc5ea8e14b6330172e14f72ef38edb10550d855d
Maria Ten Rodriguez <materod@upvnet.upv.es>
2024-06-27 00:00:47 +0200

    Init project

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la página principal</h1>
+      // Contenido de tu página principal aquí
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json']
+  }
+};
